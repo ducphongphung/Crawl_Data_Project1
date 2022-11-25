28daf2b12a4801a462de0f2cda37db5392d66450
ducphongphung <ducphongtester02@gmail.com>
2022-11-25 15:57:29 +0700

    update

--- Crawl_data.py
+++ Crawl_data.py
@@ -2,13 +2,24 @@ import json
 import os
 import cfscrape
 from bs4 import BeautifulSoup
+from datetime import datetime
+
 
 #bypass cloudflare
 scraper = cfscrape.create_scraper()
 
+def writelog(logstring):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    
+    with open('./error_log_nettruyentop.txt', 'a', encoding='utf8') as f:
+        f.write(current_time + '\t' + logstring+'\n')
+    f.close()
+
+
 def dumpData(data):
-    # C:\Users\Duc Phong Phung\Project_1\Crawl_Nettruyen\Crawl_Data_Project1\nettruyenin.json
-    path_json = "C:\\Users\\Duc Phong Phung\\Project_1\\Crawl_Nettruyen\\Crawl_Data_Project1\\nettruyenin.json"
+    # C:\Users\Duc Phong Phung\Project_1\Crawl_Nettruyen\Crawl_Data_Project1\nettruyenon.json
+    path_json = "C:\\Users\\Duc Phong Phung\\Project_1\\Crawl_Nettruyen\\Crawl_Data_Project1\\nettruyenon.json"
     with open(path_json, 'w') as f:
         json.dump(data, f, ensure_ascii = True)
     f.close()
@@ -38,52 +49,53 @@ def getDetailItem(address):
     return detail
 
 # download 1 episode
-# def DownloadImages(address, folder): # folder name episode
-#     page_soup = BeautifulSoup(scraper.get(address).content, 'html.parser')
-#     image_list = page_soup.find('main', {"class": "main"}).find('div', {"class": "reading-detail box_doc"}).findAll('div', {"class": "page-chapter"})
-#     images = []
-#     for image in image_list:
-#         item = {
-#             "alt": image.find('img').attrs['alt'],
-#             "data-original": image.find('img').attrs['data-original'],
-#             "src": image.find('img').attrs['src'],
-#             "data-index": image.find('img').attrs['data-index'],
-#         }
-#         images.append(item)
+def DownloadImages(address, folder): # folder name episode
+    page_soup = BeautifulSoup(scraper.get(address).content, 'html.parser')
+    image_list = page_soup.find('main', {"class": "main"}).find('div', {"class": "reading-detail box_doc"}).findAll('div', {"class": "page-chapter"})
+    images = []
+    for image in image_list:
+        item = {
+            "alt": image.find('img').attrs['alt'],
+            "data-original": image.find('img').attrs['data-original'],
+            "src": image.find('img').attrs['src'],
+            "data-index": image.find('img').attrs['data-index'],
+        }
+        images.append(item)
 
-#         # download to folder:
-#         if "http" in item['src'][:5]:
-#             download_image(item['alt'], folder, item['src'])
-#         else:
-#             download_image(item['alt'], folder, "http:"+item['src'])
+        # download to folder:
+        if "http" in item['src'][:5]:
+            download_image(item['alt'], folder, item['src'])
+        else:
+            download_image(item['alt'], folder, "http:"+item['src'])
 
-#     return images
+    return images
 
-# def download_image(namefile, folder, url):
-#     fixed_name = folder+"/" + "".join(x for x in namefile if (x.isalnum() or x=='.' or x == '_' or x == ' '))
-#     with open(fixed_name + '.jpg', 'wb') as handle:
-#         try:
-#             response = scraper.get(url, headers={'referer': 'https://www.nettruyenin.com/'})
-#             if not response.ok:
-#                 print("Warning response!")
-#                 print(response)
+# download 1 image
+def download_image(namefile, folder, url):
+    fixed_name = folder+"/" + "".join(x for x in namefile if (x.isalnum() or x=='.' or x == '_' or x == ' '))
+    with open(fixed_name + '.jpg', 'wb') as handle:
+        try:
+            response = scraper.get(url, headers={'referer': 'https://www.nettruyenon.com/'})
+            if not response.ok:
+                print("Warning response!")
+                print(response)
 
-#             for block in response.iter_content(1024):
-#                 if not block:
-#                     break
-#                 handle.write(block)
-#             handle.close()
-#         except Exception as e:
-#             print("Exception: ", e)
-#             handle.close()
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+            handle.close()
+        except Exception as e:
+            print("Exception: ", e)
+            handle.close()
     
 
 # Run once 
 def GetItems(index):
-    address_sub = "https://www.nettruyenin.com/tim-truyen-nang-cao?page={page_index}"
+    address_sub = "https://www.nettruyenon.com/tim-truyen-nang-cao?page={page_index}"
 
     if index == 1:
-        address = "https://www.nettruyenin.com/tim-truyen-nang-cao"
+        address = "https://www.nettruyenon.com/tim-truyen-nang-cao"
     else:
         address = address_sub.format(page_index=index)
 
@@ -92,9 +104,9 @@ def GetItems(index):
     items_content = page.find('div', {"class": "items"}).findAll('div', {"class": "item"})
     for item in items_content:
         item_info = {
+            "title": item.find('div', {"class": "title"}).text.strip(),
             "link": item.find('div', {"class": "image"}).find('a').attrs['href'],
             "image": item.find('div', {"class": "image"}).find('img').attrs['data-original'],
-            "title": item.find('div', {"class": "title"}).text.strip(),
             "description": item.find('div', {"class": "box_text"}).text.strip()
         }
 
@@ -104,10 +116,79 @@ def GetItems(index):
         items.append(item_info)
     return items
 
+# Download All Episode in 1 Comic
+def DownloadAllEpisodes(parent_folder, episodes_list):
+    # make folder
+    for episode in episodes_list:
+        episode_folder = parent_folder + '/' +  "".join(x for x in episode['name'] if (x.isalnum() or x=='.' or x == '_' or x == ' '))
+        if not os.path.isdir(episode_folder):
+            os.mkdir(episode_folder)
+        try:
+            print("Start download: ", episode['name'])
+            DownloadImages(episode['link'], episode_folder)
+        except Exception as e:
+            print(episode['name'])
+            print(e)
+            print("Download Episode Error")
+            writelog(episode['name'] + ":\t" + str(e))
+
+
+def DownloadEpisodeWithIndex(parent_folder, episodes_list, index): # index is index of episode in episodes_list
+    episode_folder = parent_folder + '/' +  "".join(x for x in episodes_list[index]['name'] if (x.isalnum() or x=='.' or x == '_' or x == ' '))
+    print("Scan special 1 episode: ", episode_folder)
+
+    if not os.path.isdir(episode_folder):
+        os.mkdir(episode_folder)
+    try:
+        DownloadImages(episodes_list[index]['link'], episode_folder)
+    except Exception as e:
+        print(episodes_list[index]['name'])
+        print(e)
+        print("Download Episode Error")
+        writelog(e)
+
+def DownloadComicsWithName(name): # using with has nettruyentop.json file
+    
+    root_path = "C:/Users/Duc Phong Phung/Project_1/Crawl_Nettruyen/Crawl_Data_Project1/Comics/"
+    if not os.path.isdir(root_path):
+        os.mkdir(root_path)
+
+    # load file data
+    with open('./nettruyentop.json', 'r') as f:
+        all_items = json.load(f)
+    f.close()
+    
+    for item in all_items:
+        if name.lower() in item['title'].lower():
+            print(item['title'])
+            # Call download
+            parent_folder = root_path + "/" + name + "/"
+            if not os.path.isdir(parent_folder):
+                os.mkdir(parent_folder)
+            parent_folder += "".join(x for x in item['title'] if (x.isalnum() or x=='.' or x == '_' or x == ' ' or x == '-'))
+            if not os.path.isdir(parent_folder):
+                os.mkdir(parent_folder)
+
+            DownloadAllEpisodes(parent_folder, item['detail']['episodes'])
+
+# Call by thread
+def DownloadAllEpisodesThread(item):
+    print("Scan comic: ", item["title"])
+
+    root_path = "C:/Users/Duc Phong Phung/Project_1/Crawl_Nettruyen/Crawl_Data_Project1/Comics/"
+    if not os.path.isdir(root_path):
+        os.mkdir(root_path)
+
+    parent_folder = root_path + "".join(x for x in item['title'] if (x.isalnum() or x=='.' or x == '_' or x == ' '))
+    if not os.path.isdir(parent_folder):
+        os.mkdir(parent_folder)
+
+    DownloadAllEpisodes(parent_folder, item['detail']['episodes'])
+
 # Run Once
 def getNetTruyenData():
     index = 1
-    max_index = 50
+    max_index = 2
     all_items = []
     for index in range(1, max_index+1): 
         try:
@@ -116,7 +197,7 @@ def getNetTruyenData():
             continue
         #dump
         all_items.append(result)
-    print("So luong truyen: " , len(all_items))
+    # print("So luong truyen: " , len(all_items))
     dumpData(all_items)
 
 
